11955fbe62dd6b94bab0191720e63df9e6995375
Duong Tuan <tuanda5353@gmail.com>
2018-11-05 09:26:41 +0700

    Update Sign Up screen

---
+++ app/src/main/java/framgia/com/ichat/utils/Constant.java
@@ -0,0 +1,7 @@
+package framgia.com.ichat.utils;
+
+public class Constant {
+    public class UserProfile{
+        public static final String DEFAULT_PROFILE_URL = "https://img.huffingtonpost.com/asset/5b7fdeab1900001d035028dc.jpeg?cache=sixpwrbb1s&ops=1910_1000";
+    }
+}
