2bbee6a8ba1e640a7e601ac31fb15956b89601c9
manhphan <manhphan49@gmail.com>
2018-11-07 14:58:09 +0700

    handle_with_item

--- app/src/main/java/framgia/com/ichat/screen/profile/ProfileActivity.java
+++ app/src/main/java/framgia/com/ichat/screen/profile/ProfileActivity.java
@@ -1,27 +1,57 @@
 package framgia.com.ichat.screen.profile;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
 import com.google.firebase.auth.FirebaseAuth;
 
 import framgia.com.ichat.GlideApp;
 import framgia.com.ichat.R;
 import framgia.com.ichat.data.model.User;
 import framgia.com.ichat.screen.base.BaseActivity;
+import framgia.com.ichat.screen.login.LoginActivity;
 
-public class ProfileActivity extends BaseActivity implements ProfileContract.View {
+public class ProfileActivity extends BaseActivity implements ProfileContract.View,
+        View.OnClickListener {
+    public static final int PICK_IMAGE_FROM_GALLERY = 0x248;
+    public static final int PICK_IMAGE_FROM_CAMERA = 0x249;
+    public static final int PERMISSIONS_REQUEST_READ_EXTERNAL = 0x141;
+    public static final int PERMISSIONS_REQUEST_CAMERA = 0x142;
+    public static final Uri EXTERNAL_CONTENT_URI = "android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI"
+    public static final String ACTION_PICK = Intent.ACTION_PICK;
+    public static final String ACTION_IMAGE_CAPTURE = MediaStore.ACTION_IMAGE_CAPTURE;
+    public static final double WIDTH = 0.90;
+    public static final double HEIGHT = 0.70;
     private static final String EXTRA_USER = "EXTRA_USER";
     private static final String SPACE = " ";
-    private TextView mTextUserName, mTextEmail, mTextLastSignIn;
-    private ImageView mImageCover, mImageViewAvatar;
-    private Button mButtonEdit, mButtonSignOut;
+    private TextView mTextUserName;
+    private TextView mTextEmail;
+    private TextView mTextLastSignIn;
+    private ImageView mImageCover;
+    private ImageView mImageViewAvatar;
+    private Button mButtonEdit;
+    private Button mButtonSignOut;
     private ProfileContract.Presenter mPresenter;
+    private Dialog mDialog;
+    private EditText mEditDialogUserName;
+    private ImageView mDialogImage;
 
     public static Intent getIntent(Context context, User user) {
         Intent intent = new Intent(context, ProfileActivity.class);
@@ -73,6 +103,131 @@ public class ProfileActivity extends BaseActivity implements ProfileContract.Vie
         mButtonSignOut.setVisibility(View.INVISIBLE);
     }
 
+    @Override
+    public boolean isCheckSelfPermission(String permission) {
+        return ContextCompat.checkSelfPermission(this,
+                permission)
+                != PackageManager.PERMISSION_GRANTED;
+    }
+
+    @Override
+    public boolean isShouldShowRequestPermission(String permission) {
+        return ActivityCompat.shouldShowRequestPermissionRationale(this, permission);
+    }
+
+    @Override
+    public void requestPermission(String permission, int requestCode) {
+        ActivityCompat.requestPermissions(this, new String[]{permission},
+                requestCode);
+    }
+
+    @Override
+    public void onPermissionError() {
+        this.showToastShort(getString(R.string.profile_permission_error));
+    }
+
+    @Override
+    public void showDialog() {
+        initDialog();
+        mPresenter.initVariable();
+        mDialog.show();
+    }
+
+    @Override
+    public void showProgress() {
+        this.showProgressDialog();
+    }
+
+    @Override
+    public void setImageDialogFromGallery(Uri uri) {
+        setImage(mDialogImage, uri.toString());
+    }
+
+    @Override
+    public void setImageDialogFromCamera(Bitmap bitmap) {
+        setImage(mDialogImage, bitmap);
+    }
+
+    @Override
+    public void chooseImageFromGallery() {
+        startActivityForResult(new Intent(ACTION_PICK, EXTERNAL_CONTENT_URI),
+                PICK_IMAGE_FROM_GALLERY);
+    }
+
+    @Override
+    public void chooseImageFromCamera() {
+        startActivityForResult(new Intent(ACTION_IMAGE_CAPTURE),
+                PICK_IMAGE_FROM_CAMERA);
+    }
+
+    @Override
+    public void onUserNameError() {
+        mEditDialogUserName.setError(getString(R.string.error_not_empty));
+    }
+
+    @Override
+    public void updateProfile(Uri uri, String name) {
+        this.hideProgressDialog();
+        mDialog.dismiss();
+        setImage(mImageViewAvatar, uri.toString());
+    }
+
+    @Override
+    public void updateProfile(String name) {
+        this.hideProgressDialog();
+        mDialog.dismiss();
+        mTextUserName.setText(name);
+    }
+
+    @Override
+    public void onUpdateError() {
+        this.showToastShort(getString(R.string.profile_edit_fail));
+    }
+
+    @Override
+    public void signOut() {
+        FirebaseAuth.getInstance().signOut();
+        navigate();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mPresenter.setImage(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_edit_profile:
+                showDialog();
+                break;
+            case R.id.button_choose_camera:
+                mPresenter.requestPermission(Manifest.permission.CAMERA,
+                        PERMISSIONS_REQUEST_CAMERA);
+                break;
+            case R.id.button_choose_gallery:
+                mPresenter.requestPermission(Manifest.permission.READ_EXTERNAL_STORAGE,
+                        PERMISSIONS_REQUEST_READ_EXTERNAL);
+                break;
+            case R.id.button_update:
+                mPresenter.updateInfo(getText(mEditDialogUserName));
+                break;
+            case R.id.button_sign_out:
+                signOut();
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        mPresenter.onRequestPermissionsResult(requestCode, grantResults);
+    }
+
     private User getUser() {
         return getIntent().getParcelableExtra(EXTRA_USER);
     }
@@ -80,6 +235,15 @@ public class ProfileActivity extends BaseActivity implements ProfileContract.Vie
     private void setImage(ImageView image, String pathImage) {
         GlideApp.with(this)
                 .load(pathImage)
+                .circleCrop()
+                .into(image);
+    }
+
+    private void setImage(ImageView image, Bitmap bitmap) {
+        GlideApp.with(this)
+                .load(bitmap)
+                .placeholder(R.drawable.ic_loading)
+                .apply(RequestOptions.diskCacheStrategyOf(DiskCacheStrategy.NONE))
                 .circleCrop()
                 .into(image);
     }
@@ -94,5 +258,27 @@ public class ProfileActivity extends BaseActivity implements ProfileContract.Vie
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+    }
+
+    private void initDialog() {
+        int width = (int) (getResources().getDisplayMetrics().widthPixels * WIDTH);
+        int height = (int) (getResources().getDisplayMetrics().heightPixels * HEIGHT);
+        mDialog = new Dialog(this);
+        mDialog.setContentView(R.layout.dialog_edit_profile);
+        mEditDialogUserName = mDialog.findViewById(R.id.edit_pro_user_name);
+        mEditDialogUserName.setText(mTextUserName.getText().toString());
+        mDialog.getWindow().setLayout(width, height);
+        mDialogImage = mDialog.findViewById(R.id.image_dialog_avatar);
+        mDialog.findViewById(R.id.button_choose_camera).setOnClickListener(this);
+        mDialog.findViewById(R.id.button_choose_gallery).setOnClickListener(this);
+        mDialog.findViewById(R.id.button_update).setOnClickListener(this);
+    }
+
+    private String getText(EditText text) {
+        return text.getText().toString();
+    }
+
+    private void navigate() {
+        startActivity(LoginActivity.getIntent(this));
     }
 }
--- app/src/main/java/framgia/com/ichat/screen/profile/ProfileContract.java
+++ app/src/main/java/framgia/com/ichat/screen/profile/ProfileContract.java
@@ -1,5 +1,8 @@
 package framgia.com.ichat.screen.profile;
 
+import android.graphics.Bitmap;
+import android.net.Uri;
+
 import com.google.firebase.auth.FirebaseUser;
 
 import framgia.com.ichat.data.model.User;
@@ -11,6 +14,36 @@ public interface ProfileContract {
         void showCoverImage();
 
         void hideButton();
+
+        boolean isCheckSelfPermission(String permission);
+
+        boolean isShouldShowRequestPermission(String permission);
+
+        void requestPermission(String permission, int requestCode);
+
+        void onPermissionError();
+
+        void showDialog();
+
+        void showProgress();
+
+        void setImageDialogFromGallery(Uri uri);
+
+        void setImageDialogFromCamera(Bitmap bitmap);
+
+        void chooseImageFromGallery();
+
+        void chooseImageFromCamera();
+
+        void onUserNameError();
+
+        void updateProfile(Uri uri, String name);
+
+        void updateProfile(String name);
+
+        void onUpdateError();
+
+        void signOut();
     }
 
     interface Presenter {
